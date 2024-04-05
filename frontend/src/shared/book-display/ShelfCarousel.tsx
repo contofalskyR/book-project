85cf53c5892d1ce9d1310feb6b176d92dc02651a
Aearsears <53102185+Aearsears@users.noreply.github.com>
2024-04-05 16:52:37 -0400

    remove todo

--- frontend/src/shared/book-display/ShelfCarousel.tsx
+++ frontend/src/shared/book-display/ShelfCarousel.tsx
@@ -147,7 +147,6 @@ export default class ShelfCarousel extends Component<
     }
     renderShelfBookByGenre(books: Book[]): Map<string, Array<ReactElement>> {
         const elements = Array<ReactElement>();
-        // TODO: make it scrollable and contain more boosk
         const maxBooksToDisplay = Math.min(books.length, 6);
         const map = new Map(); // book genres to list of shelfbooks
         for (let i = 0; i < maxBooksToDisplay; i++) {
